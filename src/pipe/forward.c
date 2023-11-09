1f19a646b145a2a7d7c6750a3225f628e69fb5a1
Aaron Alvarez <aaron1@cs.utexas.edu>
2023-11-09 17:35:10 -0600

    week2.24

--- src/pipe/forward.c
+++ src/pipe/forward.c
@@ -19,120 +19,120 @@ comb_logic_t forward_reg(uint8_t D_src1, uint8_t D_src2, uint8_t X_dst, uint8_t 
             uint64_t W_val_mem, bool M_wval_sel, bool W_wval_sel, bool X_w_enable,
                 bool M_w_enable, bool W_w_enable,
                     uint64_t *val_a, uint64_t *val_b){
-    // if(guest.proc->w_insn->in.w->W_sigs.w_enable){
-    //     if(D_src1 == W_dst){
+    if(guest.proc->w_insn->in.w->W_sigs.w_enable){
+        if(D_src1 == W_dst){
 
-    //         if(W_wval_sel){
-    //             if(guest.proc->w_insn->in.w->W_sigs.dst_sel){
-    //                 *val_a = guest.proc->d_insn->in.d->seq_succ_PC;
-    //             }else{
-    //                 *val_a = W_val_ex;
-    //             }
-    //         }else{
-    //             *val_a = W_val_mem;
-    //         }
-    //     }
+            if(W_wval_sel){
+                if(guest.proc->w_insn->in.w->W_sigs.dst_sel){
+                    *val_a = guest.proc->d_insn->in.d->seq_succ_PC;
+                }else{
+                    *val_a = W_val_ex;
+                }
+            }else{
+                *val_a = W_val_mem;
+            }
+        }
 
-    //     if(D_src2 == W_dst){
-    //         if(W_wval_sel){
-    //             *val_b = W_val_ex;
-    //         }else{
-    //             *val_b = W_val_mem;
-    //         }
-    //     }
-    // }
+        if(D_src2 == W_dst){
+            if(W_wval_sel){
+                *val_b = W_val_ex;
+            }else{
+                *val_b = W_val_mem;
+            }
+        }
+    }
 
-    // //Checks Memory 
-    // if(guest.proc->m_insn->in.w->W_sigs.w_enable){
-    //     if(D_src1 == M_dst){
+    //Checks Memory 
+    if(guest.proc->m_insn->in.w->W_sigs.w_enable){
+        if(D_src1 == M_dst){
 
-    //         if(M_wval_sel){
-    //             *val_a = M_val_ex;
-    //         }else{
-    //             *val_a = M_val_mem;
-    //         }
-    //     }
+            if(M_wval_sel){
+                *val_a = M_val_ex;
+            }else{
+                *val_a = M_val_mem;
+            }
+        }
 
-    //     if(D_src2 == M_dst){
-    //         if(M_wval_sel){
-    //             *val_b = M_val_ex;
-    //         }else{
-    //             *val_b = M_val_mem;
-    //         }
-    //     }
-    // }
+        if(D_src2 == M_dst){
+            if(M_wval_sel){
+                *val_b = M_val_ex;
+            }else{
+                *val_b = M_val_mem;
+            }
+        }
+    }
 
-    // //Checks execute
-    // if(guest.proc->x_insn->in.w->W_sigs.w_enable 
-    //     && !(guest.proc->x_insn->in.m->M_sigs.dmem_read)){
+    //Checks execute
+    if(guest.proc->x_insn->in.w->W_sigs.w_enable 
+        && !(guest.proc->x_insn->in.m->M_sigs.dmem_read)){
         
-    //     if(D_src1 == X_dst){
-    //         *val_a = X_val_ex;
-    //     }
-
-    //     if(D_src2 == X_dst){
-    //         *val_b = X_val_ex;
-    //     }
-    // }
-
-    // return;
-    
-    
-    
-    if (X_w_enable)
-    {
-        if (X_dst == D_src1)
-        {
+        if(D_src1 == X_dst){
             *val_a = X_val_ex;
         }
-        else if (W_dst == D_src1 && W_w_enable)
-        {
-            *val_a = W_val_ex;
-        }
-        else if (M_dst == D_src1 && M_w_enable)
-        {
-            *val_a = M_wval_sel ? M_val_mem : M_val_ex;
-        }
 
-        if (X_dst == D_src2)
-        {
+        if(D_src2 == X_dst){
             *val_b = X_val_ex;
         }
-        else if (W_dst == D_src2 && W_w_enable)
-        {
-            *val_b = W_val_ex;
-        }
-        else if (M_dst == D_src2 && M_w_enable)
-        {
-            *val_b = M_wval_sel ? M_val_mem : M_val_ex;
-        }
     }
-    else
-    {
-        if (W_w_enable)
-        {
-            if (W_dst == D_src1)
-            {
-                *val_a = W_wval_sel ? W_val_mem : W_val_ex;
-            }
-            if (W_dst == D_src2)
-            {
-                *val_b = W_wval_sel ? W_val_mem : W_val_ex;
-            }
-        }
-        if (M_w_enable)
-        {
-            if (M_dst == D_src1)
-            {
-                *val_a = M_wval_sel ? M_val_mem : M_val_ex;
-            }
-            if (M_dst == D_src2)
-            {
-                *val_b = M_wval_sel ? M_val_mem : M_val_ex;
-            }
-        }
-    }
+
     return;
+    
+    
+    
+    // if (X_w_enable)
+    // {
+    //     if (X_dst == D_src1)
+    //     {
+    //         *val_a = X_val_ex;
+    //     }
+    //     else if (W_dst == D_src1 && W_w_enable)
+    //     {
+    //         *val_a = W_val_ex;
+    //     }
+    //     else if (M_dst == D_src1 && M_w_enable)
+    //     {
+    //         *val_a = M_wval_sel ? M_val_mem : M_val_ex;
+    //     }
+
+    //     if (X_dst == D_src2)
+    //     {
+    //         *val_b = X_val_ex;
+    //     }
+    //     else if (W_dst == D_src2 && W_w_enable)
+    //     {
+    //         *val_b = W_val_ex;
+    //     }
+    //     else if (M_dst == D_src2 && M_w_enable)
+    //     {
+    //         *val_b = M_wval_sel ? M_val_mem : M_val_ex;
+    //     }
+    // }
+    // else
+    // {
+    //     if (W_w_enable)
+    //     {
+    //         if (W_dst == D_src1)
+    //         {
+    //             *val_a = W_wval_sel ? W_val_mem : W_val_ex;
+    //         }
+    //         if (W_dst == D_src2)
+    //         {
+    //             *val_b = W_wval_sel ? W_val_mem : W_val_ex;
+    //         }
+    //     }
+    //     if (M_w_enable)
+    //     {
+    //         if (M_dst == D_src1)
+    //         {
+    //             *val_a = M_wval_sel ? M_val_mem : M_val_ex;
+    //         }
+    //         if (M_dst == D_src2)
+    //         {
+    //             *val_b = M_wval_sel ? M_val_mem : M_val_ex;
+    //         }
+    //     }
+    // }
+    // return;
 }
   
 
