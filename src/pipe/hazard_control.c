d9d37f97302c898e32262cc50f77542ac20f4196
Aaron Alvarez <aaron1@cs.utexas.edu>
2023-11-08 18:25:35 -0600

    week2.6

--- src/pipe/hazard_control.c
+++ src/pipe/hazard_control.c
@@ -95,131 +95,102 @@ bool check_load_use_hazard(opcode_t D_opcode, uint8_t D_src1, uint8_t D_src2,
 }
 
 comb_logic_t handle_hazards(opcode_t D_opcode, uint8_t D_src1, uint8_t D_src2,
-                            opcode_t X_opcode, uint8_t X_dst, bool X_condval)
-{
+                            opcode_t X_opcode, uint8_t X_dst, bool X_condval){
 
-    // Handle non-nominal status by stalling appropriate pipeline stages
-    if (W_out->status != STAT_AOK && W_out->status != STAT_BUB)
-    {
-        stall_pipeline();
-    }
-    if (M_out->status != STAT_AOK && M_out->status != STAT_BUB)
-    {
-        X_in->X_sigs.set_CC = false;
-        stall_pipeline_except_wb();
-    }
-    if (X_out->status != STAT_AOK && X_out->status != STAT_BUB)
-    {
-        stall_pipeline_up_to_execute();
-    }
-    if (D_out->status != STAT_AOK && D_out->status != STAT_BUB)
-    {
-        X_in->W_sigs.w_enable = false;
-        stall_pipeline_up_to_decode();
-    }
-    if (F_out->status != STAT_AOK && F_out->status != STAT_BUB)
-    {
+    comb_logic_t logic = {0};
+    bool f_stall = F_out->status == STAT_HLT || F_out->status == STAT_INS;
+    pipe_control_stage(S_FETCH, false, f_stall);
+    pipe_control_stage(S_DECODE, false, false);
+    pipe_control_stage(S_EXECUTE, false, false);
+    pipe_control_stage(S_MEMORY, false, false);
+    pipe_control_stage(S_WBACK, false, false);
+
+    // Check for a return hazard
+    if(check_ret_hazard(D_opcode)){
+        // Stall Fetch and Decode, waiting for branch resolution
         pipe_control_stage(S_FETCH, false, true);
+        pipe_control_stage(S_DECODE, false, true);
+        pipe_control_stage(S_EXECUTE, true, false);
+        pipe_control_stage(S_MEMORY, true, false);
+       
+    }else if (check_mispred_branch_hazard(X_opcode, X_condval)){
+        // Checking for Mispredicted Branch Hazard, bubble the stage
+        // affected by the misprediction
+        pipe_control_stage(S_FETCH, true, false);
+        pipe_control_stage(S_DECODE, true, false);
+        pipe_control_stage(S_EXECUTE, true, false);
+    }else if (check_load_use_hazard(D_opcode, D_src1, D_src2, X_opcode, X_dst)){
+        // Check for load-use hazard
+        // Stall decode, waiting for the load to complete
+        pipe_control_stage(S_FETCH, false, true);
+        pipe_control_stage(S_DECODE, false, true);
+        
     }
 
-    // Handle data memory operations in flight by stalling and bubbling WB
-    if (dmem_status == IN_FLIGHT)
-    {
-        stall_pipeline();
-        bubble_wb();
-    }
+    // if (W_out->status != STAT_AOK && W_out->status != STAT_BUB)
+    // { // w out: f, d, x, w
+    //     pipe_control_stage(S_FETCH, false, true);
+    //     pipe_control_stage(S_DECODE, false, true);
+    //     pipe_control_stage(S_EXECUTE, false, true);
+    //     pipe_control_stage(S_MEMORY, false, true);
+    //     pipe_control_stage(S_WBACK, false, true);
+    // }
+    // if (M_out->status != STAT_AOK && M_out->status != STAT_BUB)
+    // { // m out: f, d, x, m; same as W->in
+    //     X_in->X_sigs.set_CC = false;
+    //     pipe_control_stage(S_FETCH, false, true);
+    //     pipe_control_stage(S_DECODE, false, true);
+    //     pipe_control_stage(S_EXECUTE, false, true);
+    //     pipe_control_stage(S_MEMORY, false, true);
+    // }
+    // if (X_out->status != STAT_AOK && X_out->status != STAT_BUB)
+    // { // x out: f, d, x
+    //     pipe_control_stage(S_FETCH, false, true);
+    //     pipe_control_stage(S_DECODE, false, true);
+    //     pipe_control_stage(S_EXECUTE, false, true);
+    // }
+    // if (D_out->status != STAT_AOK && D_out->status != STAT_BUB)
+    // {
+    //     X_in->W_sigs.w_enable = false;
+    //     pipe_control_stage(S_FETCH, false, true);
+    //     pipe_control_stage(S_DECODE, false, true);
+    // }
+    // if (F_out->status != STAT_AOK && F_out->status != STAT_BUB)
+    // {
+    //     pipe_control_stage(S_FETCH, false, true);
+    // }
 
-    // Hazard checks and respective pipeline adjustments
-    if (check_load_use_hazard(D_opcode, D_src1, D_src2, X_opcode, X_dst))
-    {
-        handle_load_use_hazard();
-    }
-    else if (check_mispred_branch_hazard(X_opcode, X_condval))
-    {
-        handle_mispred_branch_hazard();
-    }
-    else if (check_ret_hazard(D_opcode))
-    {
-        handle_ret_hazard();
-    }
-}
+    // if (dmem_status == IN_FLIGHT)
+    // {
+    //     pipe_control_stage(S_FETCH, false, true);
+    //     pipe_control_stage(S_DECODE, false, true);
+    //     pipe_control_stage(S_EXECUTE, false, true);
+    //     pipe_control_stage(S_MEMORY, false, true);
+    //     pipe_control_stage(S_WBACK, true, false);
+    // }
 
-// Stall the entire pipeline
-void stall_pipeline()
-{
-    pipe_control_stage(S_FETCH, false, true);
-    pipe_control_stage(S_DECODE, false, true);
-    pipe_control_stage(S_EXECUTE, false, true);
-    pipe_control_stage(S_MEMORY, false, true);
-    pipe_control_stage(S_WBACK, false, true);
-}
-
-// Stall the pipeline except for WB
-void stall_pipeline_except_wb()
-{
-    pipe_control_stage(S_FETCH, false, true);
-    pipe_control_stage(S_DECODE, false, true);
-    pipe_control_stage(S_EXECUTE, false, true);
-    pipe_control_stage(S_MEMORY, false, true);
-    // Do not stall WB stage
-}
-
-// Stall the pipeline up to the execute stage
-void stall_pipeline_up_to_execute()
-{
-    pipe_control_stage(S_FETCH, false, true);
-    pipe_control_stage(S_DECODE, false, true);
-    pipe_control_stage(S_EXECUTE, false, true);
-    // Do not stall Memory and WB stages
-}
-
-// Stall the pipeline up to the decode stage
-void stall_pipeline_up_to_decode()
-{
-    pipe_control_stage(S_FETCH, false, true);
-    pipe_control_stage(S_DECODE, false, true);
-    // Do not stall Execute, Memory, and WB stages
-}
-
-// Bubble the WB stage
-void bubble_wb()
-{
-    pipe_control_stage(S_WBACK, true, false);
-}
-
-// Handle load-use hazard
-void handle_load_use_hazard()
-{
-    // Implementation would go here. Typically, this would involve stalling
-    // the pipeline until the data is ready to be used.
-    pipe_control_stage(S_FETCH, false, true);
-    pipe_control_stage(S_DECODE, false, true);
-    pipe_control_stage(S_EXECUTE, true, false); // Insert bubble here to resolve the hazard
-}
-
-// Handle mispredicted branch hazard
-void handle_mispred_branch_hazard()
-{
-    // Implementation would go here. Often involves flushing the pipeline and fetching the correct path.
-    flush_pipeline();
-    // Possibly set up for fetching the correct branch path
-}
-
-// Handle return instruction hazard
-void handle_ret_hazard()
-{
-    // Similar to branch misprediction, this might involve flushing the pipeline
-    flush_pipeline();
-    // Setup for the correct return address fetch, if necessary
-}
-
-// Flush the entire pipeline, typically used in branch prediction hazards
-void flush_pipeline()
-{
-    // This is a simplification, flushing should reset the pipeline but may need more logic
-    pipe_control_stage(S_FETCH, true, false);
-    pipe_control_stage(S_DECODE, true, false);
-    pipe_control_stage(S_EXECUTE, true, false);
-    pipe_control_stage(S_MEMORY, true, false);
-    pipe_control_stage(S_WBACK, true, false);
+    // if (check_load_use_hazard(D_opcode, D_src1, D_src2, X_opcode, X_dst))
+    // {
+    //     pipe_control_stage(S_FETCH, false, true);
+    //     pipe_control_stage(S_DECODE, false, true);
+    //     pipe_control_stage(S_EXECUTE, true, false);
+    //     pipe_control_stage(S_MEMORY, false, false);
+    //     pipe_control_stage(S_WBACK, false, false);
+    // }
+    // else if (check_mispred_branch_hazard(X_opcode, X_condval))
+    // {
+    //     pipe_control_stage(S_FETCH, false, false);
+    //     pipe_control_stage(S_DECODE, true, false);
+    //     pipe_control_stage(S_EXECUTE, true, false);
+    //     pipe_control_stage(S_MEMORY, false, false);
+    //     pipe_control_stage(S_WBACK, false, false);
+    // }
+    // else if (check_ret_hazard(D_opcode))
+    // {
+    //     pipe_control_stage(S_FETCH, false, false);
+    //     pipe_control_stage(S_DECODE, true, false);
+    //     pipe_control_stage(S_EXECUTE, false, false);
+    //     pipe_control_stage(S_MEMORY, false, false);
+    //     pipe_control_stage(S_WBACK, false, false);
+    // }
 }
