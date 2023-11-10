ddffdef2085b4ac8012f2f3fd952ed9e78d7e2cc
insert-aaron <59901653+insert-aaron@users.noreply.github.com>
2023-11-09 22:58:07 -0600

    Update forward.c

--- src/pipe/forward.c
+++ src/pipe/forward.c
@@ -1,9 +1,3 @@
-/**************************************************************************
- * C S 429 system emulator
- *
- * forward.c - The value forwarding logic in the pipelined processor.
- **************************************************************************/
-
 #include <stdbool.h>
 #include "forward.h"
 #include "machine.h"
@@ -15,70 +9,69 @@ extern machine_t guest;
  * execute, memory, and writeback back to decode.
  */
 comb_logic_t forward_reg(uint8_t D_src1, uint8_t D_src2, uint8_t X_dst, uint8_t M_dst, uint8_t W_dst,
-        uint64_t X_val_ex, uint64_t M_val_ex, uint64_t M_val_mem, uint64_t W_val_ex,
-            uint64_t W_val_mem, bool M_wval_sel, bool W_wval_sel, bool X_w_enable,
-                bool M_w_enable, bool W_w_enable,
-                    uint64_t *val_a, uint64_t *val_b){
-    if(guest.proc->w_insn->in.w->W_sigs.w_enable){
-        if(D_src1 == W_dst){
+                         uint64_t X_val_ex, uint64_t M_val_ex, uint64_t M_val_mem, uint64_t W_val_ex,
+                         uint64_t W_val_mem, bool M_wval_sel, bool W_wval_sel, uint64_t *val_a, uint64_t *val_b)
+{
+    // checks writeback
+    if (guest.proc->w_insn->in->W_sigs.w_enable)
+    {
 
-            if(W_wval_sel){
-                if(guest.proc->w_insn->in.w->W_sigs.dst_sel){
-                    *val_a = guest.proc->d_insn->in.d->seq_succ_PC;
-                }else{
-                    *val_a = W_val_ex;
+        if (D_src1 == W_dst)
+        {
+            if (W_wval_sel)
+            {
+                if (guest.proc->w_insn->in->W_sigs.dst_sel)
+                {
+                    *val_a = guest.proc->w_insn->in->seq_succ_PC;
                 }
-            }else{
-                *val_a = W_val_mem;
+                else *val_a = W_val_ex;
             }
+            else *val_a = W_val_mem;
         }
 
-        if(D_src2 == W_dst){
-            if(W_wval_sel){
+        if (D_src2 == W_dst)
+        {
+            if (W_wval_sel)
+            {
                 *val_b = W_val_ex;
-            }else{
-                *val_b = W_val_mem;
             }
+            else *val_b = W_val_mem;
         }
     }
 
-    //Checks Memory 
-    if(guest.proc->m_insn->in.w->W_sigs.w_enable){
-        if(D_src1 == M_dst){
+    // checks memory
+    if (guest.proc->m_insn->in->W_sigs.w_enable)
+    {
 
-            if(M_wval_sel){
+        if (D_src1 == M_dst)
+        {
+            if (M_wval_sel)
+            {
                 *val_a = M_val_ex;
-            }else{
-                *val_a = M_val_mem;
             }
+            else *val_a = M_val_mem;
         }
 
-        if(D_src2 == M_dst){
-            if(M_wval_sel){
+        if (D_src2 == M_dst)
+        {
+            if (M_wval_sel)
+            {
                 *val_b = M_val_ex;
-            }else{
-                *val_b = M_val_mem;
             }
+            else *val_b = M_val_mem;
         }
     }
 
-    //Checks execute
-    if(guest.proc->x_insn->in.w->W_sigs.w_enable 
-        && !(guest.proc->x_insn->in.m->M_sigs.dmem_read)){
-        
-        if(D_src1 == X_dst){
+    // checks execute
+    if (guest.proc->x_insn->in->W_sigs.w_enable && !guest.proc->x_insn->in->M_sigs.dmem_read)
+    {
+        if (D_src1 == X_dst)
+        {
             *val_a = X_val_ex;
         }
 
-        if(D_src2 == X_dst){
+        if (D_src2 == X_dst)
+        {
             *val_b = X_val_ex;
         }
     }
-
-    return;
-    
-    
-    
-}
-  
-
