146fa9c5ed9b4425c2f319e63077bdc4a685ec95
insert-aaron <59901653+insert-aaron@users.noreply.github.com>
2023-11-09 23:55:47 -0600

    Update forward.c

--- src/pipe/forward.c
+++ src/pipe/forward.c
@@ -12,48 +12,58 @@ comb_logic_t forward_reg(uint8_t D_src1, uint8_t D_src2, uint8_t X_dst, uint8_t 
                          uint64_t X_val_ex, uint64_t M_val_ex, uint64_t M_val_mem, uint64_t W_val_ex,
                          uint64_t W_val_mem, bool M_wval_sel, bool W_wval_sel, uint64_t *val_a, uint64_t *val_b)
 {
-   if(W_w_enable){
-        if(W_dst == D_src1) {
-            *val_a = W_val_ex; 
-        } 
-        if(W_dst == D_src2) {
+   if (X_w_enable)
+    {
+        if (X_dst == D_src1)
+        {
+            *val_a = X_val_ex;
+        }
+        else if (W_dst == D_src1 && W_w_enable)
+        {
+            *val_a = W_val_ex;
+        }
+        else if (M_dst == D_src1 && M_w_enable)
+        {
+            *val_a = M_wval_sel ? M_val_mem : M_val_ex;
+        }
+
+        if (X_dst == D_src2)
+        {
+            *val_b = X_val_ex;
+        }
+        else if (W_dst == D_src2 && W_w_enable)
+        {
             *val_b = W_val_ex;
         }
-    } 
-    if(M_w_enable){
-        if(M_dst == D_src1) { 
-            *val_a = M_val_ex; 
-        } 
-        if(M_dst == D_src2) {
-            *val_b = M_val_ex;
-        } 
-    } 
-    if(X_w_enable){
-        if(X_dst == D_src1) { //x dest = dx
-            *val_a = X_val_ex; //val_A = M_in->val_a && vx = X_val_ex
-        } //don't need an else cuz regval_a = val_a
-        if(X_dst == D_src2) {
-            *val_b = X_val_ex;
-        } //don't need an else cuz regval_b = val_b
-    } 
-
-    //load-use hazards 
-    if(W_wval_sel){
-        if(W_dst == D_src1) {
-            *val_a = W_val_mem; 
-        } 
-        if(W_dst == D_src2) {
-            *val_b = W_val_mem;
+        else if (M_dst == D_src2 && M_w_enable)
+        {
+            *val_b = M_wval_sel ? M_val_mem : M_val_ex;
         }
-    } 
-    if(M_wval_sel){
-        if(M_dst == D_src1) { 
-            *val_a = M_val_mem; 
-        } 
-        if(M_dst == D_src2) {
-            *val_b = M_val_mem;
-        } 
-    } 
-    
+    }
+    else
+    {
+        if (W_w_enable)
+        {
+            if (W_dst == D_src1)
+            {
+                *val_a = W_wval_sel ? W_val_mem : W_val_ex;
+            }
+            if (W_dst == D_src2)
+            {
+                *val_b = W_wval_sel ? W_val_mem : W_val_ex;
+            }
+        }
+        if (M_w_enable)
+        {
+            if (M_dst == D_src1)
+            {
+                *val_a = M_wval_sel ? M_val_mem : M_val_ex;
+            }
+            if (M_dst == D_src2)
+            {
+                *val_b = M_wval_sel ? M_val_mem : M_val_ex;
+            }
+        }
+    }
     return;
 }
